b5cbc45231623fd9840162ec921f60fad03d4e49
niaranaind <niaranaid15@gmail.com>
2024-04-11 00:28:05 +0530

    updating info

--- Recursion_Problem/nth_stairs_problem.cpp
+++ Recursion_Problem/nth_stairs_problem.cpp
@@ -1,3 +1,13 @@
+/*Problem statement
+You have been given a number of stairs. Initially, you are at the 0th stair, and you need to reach the Nth stair.
+
+
+
+Each time, you can climb either one step or two steps.
+
+
+
+You are supposed to return the number of distinct ways you can climb from the 0th step to the Nth step.*/
 #include<iostream>
 using namespace std;
 
