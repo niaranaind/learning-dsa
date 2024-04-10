36fd7b66fc1600e67de14f1b0083783d77971c25
niaranaind <niaranaid15@gmail.com>
2024-04-11 00:53:11 +0530

    Recursion new question

---
+++ Recursion_Problem/check_sortArray.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+class solution{
+
+    public:
+
+    bool isSorted(int arr[],int size){
+
+        //base case
+        if(size==0 || size==1){
+            return true;
+        }
+
+        if(arr[0]>arr[1]){
+
+            return false;
+        }
+        else{
+
+            bool ans=isSorted(arr+1,size-1);
+            return ans;
+        }
+    }
+};
+
+int main(){
+
+    int arr[100];
+    int size;
+
+    cout<<"enter the size of array: ";
+    cin>>size;
+
+    cout<<"enter the element in array: ";
+    for(int i=0;i<size;i++){
+
+        cin>>arr[i];
+    }
+
+    solution sr;
+
+    if(sr.isSorted(arr,size)){
+
+        cout<<"the array is sorted "<<endl;
+    }
+    else{
+
+        cout<<"the array is Not sorted "<<endl;
+    }
+
+    return 0;
+}
+
